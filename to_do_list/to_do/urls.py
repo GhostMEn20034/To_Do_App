366a9bef9636141db9c1e0e49fff185d33b30224
GhostMEn20034 <yukon197415@gmail.com>
2022-08-31 19:54:35 +0300

    add transition to tasks through the categories

--- to_do_list/to_do/urls.py
+++ to_do_list/to_do/urls.py
@@ -5,5 +5,5 @@ app_name = "to_do"
 urlpatterns = [
     path('', views.RedirectToBasePageView.as_view(), name='redirect-to-base-page'),
     path('category/', views.CategoryView.as_view(), name="category"),
-    path('<int:category_id>/todo/', views.todo, name="to-do-list"),
+    path('category/<int:category_id>/', views.Todos.as_view(), name="to-do-list"),
 ]
--- to_do_list/to_do/views.py
+++ to_do_list/to_do/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views.generic.base import RedirectView
 from .models import Task, Category
 from django.views import View
@@ -16,11 +16,11 @@ class RedirectToBasePageView(RedirectView):
 class CategoryView(View):
     template_name = "to_do/category.html"
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
         categories = Category.objects.all()
         return render(request, self.template_name, {"categories": categories})
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         name = request.POST["name"]
         category = Category(name=name)
         category.save()
@@ -29,7 +29,19 @@ class CategoryView(View):
 
 
 
-def todo(request):
-    todos = Task.objects.all()  # get all Task objects
-    categories = Category.objects.all()  # get all Category objects
-    return render(request, "to_do/todo.html", {"todos": todos, "categories": categories})
+
+class Todos(View):
+    template_name = "to_do/todo.html"
+
+    def get(self, request, category_id):
+        category = get_object_or_404(Category, id=category_id)  # get all Category objects
+        try:
+            todos = Task.objects.filter(category_id=category_id)# get all Task objects
+        except (KeyError, Task.DoesNotExist):
+            todos = None
+        context = {
+            "does_not_exist_msg": "There are no todos",
+            "category": category,
+            "todos": todos,
+                }
+        return render(request, self.template_name, context)
