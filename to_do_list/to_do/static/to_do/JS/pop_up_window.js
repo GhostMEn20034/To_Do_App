3c4bf86b33576ff152a05c058884fb391ac598b7
GhostMEn20034 <yukon197415@gmail.com>
2022-10-17 18:05:33 +0300

    Rewrite html template for Category view

--- to_do_list/to_do/static/to_do/JS/pop_up_window.js
+++
@@ -1,8 +0,0 @@
-document.querySelector(
-    "#show-window").addEventListener("click", function () {
-    document.querySelector(".pop_up_window").classList.add("active");
-});
-document.querySelector(".pop_up_window .close-btn").addEventListener(
-    "click", function () {
-        document.querySelector(".pop_up_window").classList.remove("active");
-    });
